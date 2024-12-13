8346b957cb38c0d7d668ff715478e4b0b416a307
anas <anaslouahid@gmail.com>
2024-12-13 17:56:45 +0100

    ajout du test 2

--- Tp2AnasLouahid/src/main/java/ma/emsi/AnasLouahid/Main2.java
+++ Tp2AnasLouahid/src/main/java/ma/emsi/AnasLouahid/Main2.java
@@ -1,18 +1,21 @@
 package ma.emsi.AnasLouahid;
 
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.request.ResponseFormat;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
 
 import java.time.Duration;
-//main pour le premier test
-public class Main1 {
+import java.util.Map;
+
+//main pour le deuxieme test
+public class Main2 {
     public static void main(String[] args) {
         String key=System.getenv("Gemini_Key");
         ChatLanguageModel model = GoogleAiGeminiChatModel.builder().apiKey(key)
                 .modelName("gemini-1.5-flash").temperature(0.5).timeout(Duration.ofSeconds(60)).build();
-        //.responseFormat(ResponseFormat.TEXT).build();
-        String reponse = model.generate("Valeur euro en dirhams ?");
+        Prompt prompt = PromptTemplate.from("traduit le mot {{mot}} en {{langue}}?").apply(Map.of("mot","bonjour","langue","allemand"));
+      String  reponse = model.generate(prompt.text());
         System.out.println(reponse);
 
     }
